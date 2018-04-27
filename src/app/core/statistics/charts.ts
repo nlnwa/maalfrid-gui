020a5baf37660bafd1e9eb5873984c37d065d461
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-04-27 15:21:26 +0200

    feat: add chartOptions for stackedArea chart and refactor tickFormat to function

--- src/app/core/statistics/charts.ts
+++ src/app/core/statistics/charts.ts
@@ -1,8 +1,9 @@
 declare let d3, nv: any;
 
+const dateFormat = (unix) => d3.time.format('%e.%m.%Y')(new Date(unix * 1000));
 
-export const options = {
-  pieChart: {
+export const chartOptions = {
+  pie: {
     chart: {
       type: 'pieChart',
       x: function (d) {
@@ -12,48 +13,55 @@ export const options = {
         return d.value;
       },
       showLabels: true,
+      donut: true,
       labelType: 'percent',
       labelThreshold: 0.01,
       labelSunbeamLayout: false,
-      duration: 100,
-      // legend: {
-      //   margin: {
-      //     top: 10,
-      //     right: 15,
-      //     bottom: 5,
-      //     left: 0
-      //   }
-      // }
+      duration: 250,
+      noData: 'Ingen data tilgjengelig.',
     }
   }
   ,
-  multiBarChart: {
+  multiBar: {
     chart: {
+      duration: 250,
       type: 'multiBarChart',
-      stacked: false,
-      margin: {
-        top: 75,
-        bottom: 75,
-        left: 75
-      },
+      stacked: true,
+      showLabels: true,
+      labelType: 'percent',
       x: (d) => d[0],
       y: (d) => d[1],
       xAxis: {
         axisLabel: 'Innhøsting',
-        tickFormat: function (d) {
-          return d3.time.format('%e.%m.%Y')(
-            new Date(d)
-          );
-        }
+        tickFormat: dateFormat,
+        axisLabelDistance: 10,
+      }
+    },
+    yAxis: {
+      axisLabel: 'Antall tekster',
+      tickFormat: (d) => d3.format(',f')(d),
+      axisLabelDistance: 12,
+    },
+  },
+
+  stackedArea: {
+    chart: {
+      duration: 250,
+      type: 'stackedAreaChart',
+      style: 'stacked',
+      useInteractiveGuideline: true,
+      x: (d) => d[0],
+      y: (d) => d[1],
+      xAxis: {
+        axisLabel: 'Innhøsting',
+        tickFormat: dateFormat,
+        axisLabelDistance: 12,
       },
       yAxis: {
-        axisLabel: 'Antall tekster' +
-        '',
-        tickFormat: function (d) {
-          return d3.format(',f')(d);
-        },
-        axisLabelDistance: 12
+        axisLabel: 'Antall tekster' + '',
+        tickFormat: (d) => d3.format(',f')(d),
+        axisLabelDistance: 12,
       },
-    }
+    },
   },
 };
